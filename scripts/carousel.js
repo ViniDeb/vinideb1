2e30998d5a08f798e10d56915e1a5d24b0eec7e2
Vinicius Moises <viniciusmoiseispereira007@gmail.com>
2024-11-27 08:25:01 -0300

    Add files via upload

---
+++ scripts/carousel.js
@@ -0,0 +1,37 @@
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+const prevButton = document.querySelector('.carousel-button.prev');
+const nextButton = document.querySelector('.carousel-button.next');
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+// Arrange slides side-by-side
+slides.forEach((slide, index) => {
+  slide.style.left = `${slideWidth * index}px`;
+});
+
+// Move to the specific slide
+const moveToSlide = (track, currentSlide, targetSlide) => {
+  track.style.transform = `translateX(-${targetSlide.style.left})`;
+  currentSlide.classList.remove('current-slide');
+  targetSlide.classList.add('current-slide');
+};
+
+// Previous button click
+prevButton.addEventListener('click', () => {
+  const currentSlide = track.querySelector('.current-slide');
+  const prevSlide = currentSlide.previousElementSibling;
+
+  if (prevSlide) {
+    moveToSlide(track, currentSlide, prevSlide);
+  }
+});
+
+// Next button click
+nextButton.addEventListener('click', () => {
+  const currentSlide = track.querySelector('.current-slide');
+  const nextSlide = currentSlide.nextElementSibling;
+
+  if (nextSlide) {
+    moveToSlide(track, currentSlide, nextSlide);
+  }
+});
